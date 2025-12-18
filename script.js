65a8929e960f0d3144e3584ebe85006ba0b290f5
Ni-sHa-nt <nk4815027@gmail.com>
2025-12-18 16:41:35 +0530

    Footer Section more responsive

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-
 const navbarToggler = document.querySelector(".navbar-toggler");
 
 navbarToggler.addEventListener('click', () => {
     navbarToggler.classList.toggle('active');
 })
+
+
+// locomotive smooth scroll
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
